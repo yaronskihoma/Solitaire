a7a6e56f10efd017e46ad990440f81d7cfb0124a
yaronskihoma <123949299+yaronskihoma@users.noreply.github.com>
2025-04-24 17:12:17 +0200

    Update Vite config for proper asset handling

--- config/vite.config.js
+++ config/vite.config.js
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   base: "./",
   build: {
     rollupOptions: {
+      input: {
+        main: resolve(__dirname, '../index.html'),
+      },
       output: {
         entryFileNames: 'assets/js/[name]-[hash].js',
+        assetFileNames: 'assets/[ext]/[name]-[hash].[ext]',
       },
     },
   },
